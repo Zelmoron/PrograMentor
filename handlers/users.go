cea170532d4cd22a891be56251e7ed386a3a71cb
AItEKS <129169524+AItEKS@users.noreply.github.com>
2025-03-11 17:09:05 +0800

    Update users.go - removed registration handler

--- handlers/users.go
+++ handlers/users.go
@@ -2,9 +2,6 @@ package handlers
 
 import "github.com/gofiber/fiber/v2"
 
-func (in *InHandlers) Registration(c *fiber.Ctx) error {
-	return c.SendString("Registration successful")
-}
 
 func (in *InHandlers) Login(c *fiber.Ctx) error {
 	return nil
