9c7bbd7c314f6e5bb0b433249753f93397587b50
Zelmoron <ak.igor13@mail.ru>
2025-04-08 12:49:56 +0800

    f

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://127.0.0.1:5500,http://localhost:8080,http://host.docker.internal:8080",
+		AllowOrigins:     "http://127.0.0.1:3000,http://localhost:8080,http://host.docker.internal:8080,http://localhost:3000",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
