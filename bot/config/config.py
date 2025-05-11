0acb786901af30b1765fc3d750fbf495fd1e7060
AItEKS <egorka.ku06@mail.ru>
2025-05-11 20:45:05 +0000

    fix

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = '8178270453:AAGcknxO2f95AOLiLSFsFF9bp2n4HqQLHNE'
 
 DB_NAME = 'mydatabase'
-DB_HOST = 'localhost'
+DB_HOST = 'db'
 DB_PASSWORD = 'password'
 DB_USER = 'user'
-DB_PORT = '5433'
+DB_PORT = '5432'
